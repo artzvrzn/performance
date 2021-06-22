b4434f97d2db32cde1043e9a1748aa78f67d5e0d
by059491 <jojoba37544@yandex.ru>
2021-06-22 23:32:13 +0300

    fixes

--- chrome_driver.py
+++ chrome_driver.py
@@ -4,21 +4,13 @@ from selenium.common.exceptions import SessionNotCreatedException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from datetime import date, timedelta
+from glob import glob
+import os
+from time import sleep
+from getpass import getuser
 from logger import logger
 
-try:
-    chrome_driver = Path(__file__).parent.resolve() / 'chromedriver_92.exe'
-    driver = webdriver.Chrome(chrome_driver)
-except SessionNotCreatedException:
-    chrome_driver = Path(__file__).parent.resolve() / 'chromedriver_91.exe'
-    driver = webdriver.Chrome(chrome_driver)
-    logger.info('Хром версии 91')
-
 LT22_URL = 'https://cuvl0301.eur.cchbc.com:8204/sap/bc/ui2/flp#LT22-display'
-
-driver.implicitly_wait(10)
-driver.get(LT22_URL)
-driver.switch_to.frame('application-LT22-display')
 
 WAREHOUSE_NUMBER = 'M0:46:::0:34'
 STORAGE_TYPE = 'M0:46:::1:34'
@@ -30,26 +22,72 @@ TO_DATE_TO = 'M0:46:::15:59'
 EXECUTE_BTN = 'M0:50::btn[8]'
 CONTINUE_BTN = 'M1:50::btn[0]'
 
-date_before = (date.today() - timedelta(days=1)).strftime('%d.%m.%Y')
+DATE_BEFORE = (date.today() - timedelta(days=1)).strftime('%d.%m.%Y')
+DATE_TODAY = date.today().strftime('%d.%m.%Y')
 
-fill_parameters = {WAREHOUSE_NUMBER: '270',
-                   STORAGE_TYPE: '200',
-                   CONFIRMED_TO: '',
-                   SOURCE: '',
-                   DESTINATION: '',
-                   TO_DATE_FROM: date_before,
-                   TO_DATE_TO: date.today().strftime('%d.%m.%Y'),
-                   EXECUTE_BTN: ''}
+options = webdriver.ChromeOptions()
+options.add_argument('--lang=en')
 
-for id_elem, value in fill_parameters.items():
-    element = driver.find_element_by_id(id_elem)
-    if id_elem in (CONFIRMED_TO, SOURCE, DESTINATION, EXECUTE_BTN):
-        element.click()
-    else:
-        element.clear()
-        element.send_keys(value)
 
-body = WebDriverWait(driver=driver, timeout=10).until(lambda d: d.find_element_by_id('M0:46:::26:1_l'))
-body.send_keys(Keys.SHIFT + Keys.F4)
+def get_last_file_path():
+    try:
+        last_file = max(glob(f'C:\\Users\\{getuser()}\\Downloads\\*.xlsx'), key=os.path.getctime)
+    except ValueError:
+        last_file = 'No file'
+        logger.debug(last_file)
+    return last_file
 
-driver.find_element_by_id(CONTINUE_BTN).click()
+
+class LT22Run:
+
+    def __init__(self):
+        self.driver = None
+        self.fill_parameters = {WAREHOUSE_NUMBER: '270',
+                                STORAGE_TYPE: '200',
+                                CONFIRMED_TO: '',
+                                SOURCE: '',
+                                DESTINATION: '',
+                                TO_DATE_FROM: DATE_BEFORE,
+                                TO_DATE_TO: DATE_TODAY,
+                                EXECUTE_BTN: ''}
+        self.file_name = get_last_file_path()
+
+    def _start_application(self):
+        try:
+            chrome_path = Path(__file__).parent.resolve() / 'chromedriver_92.exe'
+            self.driver = webdriver.Chrome(chrome_path)
+            logger.info(f'Chrome version {self.driver.capabilities["browserVersion"]}')
+        except SessionNotCreatedException:
+            chrome_path = Path(__file__).parent.resolve() / 'chromedriver_91.exe'
+            self.driver = webdriver.Chrome(chrome_path)
+            logger.info(f'Chrome version {self.driver.capabilities["browserVersion"]}')
+        self.driver.implicitly_wait(10)
+        self.driver.get(LT22_URL)
+        self.driver.switch_to.frame('application-LT22-display')
+
+    def _get_file_path(self):
+        while self.file_name == get_last_file_path():
+            sleep(0.5)
+        self.file_name = get_last_file_path()
+        logger.info(f'lt22.xlsx: {self.file_name}')
+        self.driver.quit()
+        return self.file_name
+
+    def _run_application(self):
+        self._start_application()
+        for id_elem, value in self.fill_parameters.items():
+            element = self.driver.find_element_by_id(id_elem)
+            if id_elem in (CONFIRMED_TO, SOURCE, DESTINATION, EXECUTE_BTN):
+                element.click()
+            else:
+                element.clear()
+                element.send_keys(value)
+
+        body = WebDriverWait(driver=self.driver, timeout=300).until(lambda d: d.find_element_by_id('M0:46:::26:1_l'))
+        body.send_keys(Keys.SHIFT + Keys.F4)
+        self.driver.find_element_by_id(CONTINUE_BTN).click()
+
+    def execute(self):
+        self._run_application()
+        self._get_file_path()
+
---
+++ plot_creator.py
@@ -0,0 +1,81 @@
+from pathlib import Path
+from logger import logger
+import pandas
+import matplotlib.pyplot as plt
+import matplotlib.dates as dts
+from users import users
+from chrome_driver import LT22Run, DATE_BEFORE, DATE_TODAY
+
+
+class LT22Reader:
+
+    def __init__(self, path_to_lt22):
+        self.lt22_df = pandas.read_excel(path_to_lt22)[:-1]
+        self.lt22_df.loc[:, 'Time'] = (self.lt22_df.loc[:, 'Confirmation date'].astype('str') +
+                                      ' ' +
+                                      self.lt22_df.loc[:, 'Confirmation time'].astype('str'))
+        self.lt22_df.loc[:, 'Time'] = pandas.to_datetime(self.lt22_df.loc[:, 'Time'])
+        self.lt22_df.rename({'Source storage unit': 'Quantity'}, axis='columns', inplace=True)
+        self.pivot_df = self._to_pivot()
+
+    def _to_pivot(self):
+        lt22_night_df = self.lt22_df[
+            (self.lt22_df['Time'] > pandas.to_datetime(f'{DATE_BEFORE} 19:00:00')) &
+            (self.lt22_df['Time'] < pandas.to_datetime(f'{DATE_TODAY} 06:59:00'))
+            ]
+        lt22_night_df.loc[:, 'Time'] = lt22_night_df.loc[:, 'Time'].dt.floor('30min')
+        print(lt22_night_df)
+        pallet_by_time = lt22_night_df.groupby(['User.1', 'Time'])['Quantity'].count()  # < Здесь
+        pallet_by_time_df = pallet_by_time.to_frame().reset_index()
+        print(pallet_by_time_df['User.1'].count())
+        # pallet_by_time_df.loc[:, 'Time'] = pallet_by_time_df['Time'].dt.floor('30min')
+
+        # pallet_by_round_time = pallet_by_time_df.groupby(['User.1', 'Time'])['Quantity'].count()
+        # pallet_by_round_time_df = pallet_by_round_time.to_frame().reset_index()
+
+        lt22_pivot = pallet_by_time_df.pivot_table(index=['Time'], columns=['User.1'], values='Quantity')
+        pivot_df = lt22_pivot.reset_index()
+        pivot_df.fillna(value=0, inplace=True)
+        # renaming users to their lastnames
+        pivot_df.rename({code: name for code, name in users.items()}, axis='columns', inplace=True)
+
+        # increasing each user's quantity by quantity before
+        for i in range(1, len(pivot_df.index)):
+            for name in pivot_df.columns:
+                if name == 'Time':
+                    pass
+                else:
+                    pivot_df.loc[i, name] += pivot_df.loc[i - 1, name]
+        logger.debug(pivot_df)
+        print(pivot_df)
+        return pivot_df
+
+    def get_plot(self):
+        # setting up style for plot
+        plt.style.use('ggplot')
+        plt.style.use('seaborn-notebook')
+        date_formatter = dts.DateFormatter('%H:%M')
+        date_locator = dts.HourLocator(interval=1)
+
+        # making plot showing amount of pallets by time for each user
+        lt22_plot = self.pivot_df.plot(
+            x='Time',
+            y=[a for a in self.pivot_df.columns if a not in ['Time']],
+            xlabel='Время',
+            ylabel='Поддоны',
+            colormap='Set1',
+        )
+
+        lt22_plot.xaxis.set_major_locator(date_locator)
+        lt22_plot.xaxis.set_major_formatter(date_formatter)
+        lt22_plot.yaxis.tick_right()
+
+        plt.show()
+
+
+if __name__ == '__main__':
+    lt22_extraction = LT22Run()
+    lt22_extraction.execute()
+    file_path = lt22_extraction.file_name
+    reader = LT22Reader(file_path)
+    reader.get_plot()
